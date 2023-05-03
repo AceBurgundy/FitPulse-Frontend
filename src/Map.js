9a19267b579b787b197efdae3b5cb626ce41bff1
AceBurgundy <Samadriansabalo99@gmail.com>
2023-05-03 09:33:29 +0800

    Minor Changes

--- src/Map.js
+++ src/Map.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { HashRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 // /pages
 import Register from './pages/Authentication/Register/Register'
@@ -16,14 +16,14 @@ function Map() {
     <>
         <Router basename="/FitPulse">
           <Routes>
-            <Route path="/FitPulse/#/" element={<Login />} />
-            <Route path="/FitPulse/#/register" element={<Register />} />
-            <Route path="/FitPulse/#/gender-prompt" element={<GenderPrompt />} />
-            <Route path="/FitPulse/#/biometrics" element={ <Biometrics />} />
-            <Route path="/FitPulse/#/weekly-goals" element={ <WeeklyGoals />} />
-            <Route path="/FitPulse/#/workout-plans" element={ <WorkoutPlans/>} />
-            <Route path="/FitPulse/#/workout-plans/:workout_id/week/list" element={<WeeklyRoutine />} />
-            <Route path="/FitPulse/#/workout-plans/:workout_id/week/day/:day_id/list" element={ <Exercises/>} />
+            <Route path="/FitPulse/" element={<Login />} />
+            <Route path="/FitPulse/register" element={<Register />} />
+            <Route path="/FitPulse/gender-prompt" element={<GenderPrompt />} />
+            <Route path="/FitPulse/biometrics" element={ <Biometrics />} />
+            <Route path="/FitPulse/weekly-goals" element={ <WeeklyGoals />} />
+            <Route path="/FitPulse/workout-plans" element={ <WorkoutPlans/>} />
+            <Route path="/FitPulse/workout-plans/:workout_id/week/list" element={<WeeklyRoutine />} />
+            <Route path="/FitPulse/workout-plans/:workout_id/week/day/:day_id/list" element={ <Exercises/>} />
           </Routes>
         </Router>
     </>

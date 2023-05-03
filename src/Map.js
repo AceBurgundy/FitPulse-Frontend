6d4a2be3afb8ed7dadd92089e62675bf865acf27
AceBurgundy <Samadriansabalo99@gmail.com>
2023-05-03 09:32:47 +0800

    Fixe react router problem.

--- src/Map.js
+++ src/Map.js
@@ -14,16 +14,16 @@ import Exercises from './pages/Exercises/Exercises'
 function Map() {
   return ( 
     <>
-        <Router>
+        <Router basename="/FitPulse">
           <Routes>
-            <Route path="/FitPulse/" element={<Login />} />
-            <Route path="/FitPulse/register" element={<Register />} />
-            <Route path="/FitPulse/gender-prompt" element={<GenderPrompt />} />
-            <Route path="/FitPulse/biometrics" element={ <Biometrics />} />
-            <Route path="/FitPulse/weekly-goals" element={ <WeeklyGoals />} />
-            <Route path="/FitPulse/workout-plans" element={ <WorkoutPlans/>} />
-            <Route path="/FitPulse/workout-plans/:workout_id/week/list" element={<WeeklyRoutine />} />
-            <Route path="/FitPulse/workout-plans/:workout_id/week/day/:day_id/list" element={ <Exercises/>} />
+            <Route path="/FitPulse/#/" element={<Login />} />
+            <Route path="/FitPulse/#/register" element={<Register />} />
+            <Route path="/FitPulse/#/gender-prompt" element={<GenderPrompt />} />
+            <Route path="/FitPulse/#/biometrics" element={ <Biometrics />} />
+            <Route path="/FitPulse/#/weekly-goals" element={ <WeeklyGoals />} />
+            <Route path="/FitPulse/#/workout-plans" element={ <WorkoutPlans/>} />
+            <Route path="/FitPulse/#/workout-plans/:workout_id/week/list" element={<WeeklyRoutine />} />
+            <Route path="/FitPulse/#/workout-plans/:workout_id/week/day/:day_id/list" element={ <Exercises/>} />
           </Routes>
         </Router>
     </>
